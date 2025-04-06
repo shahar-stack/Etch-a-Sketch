47f7793d34fb864d72325f6d354f1f48feea0fec
shahar-stack <32692649+shahar-stack@users.noreply.github.com>
2025-04-06 05:57:57 +0300

    Add function that lets user change grid size
    The function prompts the user to input a number (between 1-100),
    it validates that the number is indeed in the expected range or else
    it will keep looping prompts until a valid input is given.
    Once a valid value is input, the current grid is removed and a new grid
    is created with the amount of squares the user specified.
    It's worth noting the squares resize themselves accordingly to fill out
    the grid space

--- myscripts.js
+++ myscripts.js
@@ -5,7 +5,7 @@ let squareDiv;
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         squareDiv = document.createElement("div");
-        squareDiv.id = "square";
+        squareDiv.className = "square";
         grid.appendChild(squareDiv);
         squareDiv.addEventListener("mousemove", colorSquare);
     }
@@ -17,13 +17,13 @@ function removeGrid() {
     }
 }
 
-function createGrid() {
+function createGrid(userInput) {
     grid = document.querySelector(".grid");
 
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+    for (let i = 0; i < userInput; i++) {
+        for (let j = 0; j < userInput; j++) {
             squareDiv = document.createElement("div");
-            squareDiv.id = "square";
+            squareDiv.className = "square";
             grid.appendChild(squareDiv);
             squareDiv.addEventListener("mousemove", colorSquare);
         }
@@ -37,9 +37,38 @@ function colorSquare(event) {
 
 const changeButton = document.querySelector("#changeButton");
 
-// function changeButtonClick(event) {
+function changeButtonClick() {
+    let userInput = prompt("Enter a positive number of squares per row/column (max 100):")
+    userInput = Number(userInput);
+    
+    let isValidInput = false;
+    while (isValidInput === false) {
+        if (userInput > 100) {
+            userInput = prompt("Invalid - please input a positive number that *isn't* greater than 100");
+            userInput = Number(userInput);
 
+        } else if (userInput <= 0) {
+            userInput = prompt("Invalid - please enter a *positive* number that's greater than 0");
+            userInput = Number(userInput);
 
-// }
+        } else if (typeof userInput != "number" || isNaN(userInput)) {
+            userInput = prompt("Invalid - please enter a *number* using digits");
+            userInput = Number(userInput);
 
-// changeButton.addEventListener("click", changeButtonClick);
+        }
+        else {
+            isValidInput = true;
+            break;
+        }
+    }
+
+    removeGrid();
+    createGrid(userInput);
+
+    const flexBasisPercent = 100/userInput; // 100 divided by how many squares the user wants, such so the flex basis is scaled appropriately.
+    document.querySelectorAll(".square").forEach(square => {
+        square.style.flexBasis = `${flexBasisPercent}%`;
+    });
+}
+
+changeButton.addEventListener("click", changeButtonClick);
